f8323f5441ec6d94a3d8f64a6f0d2b1b6771f424
Alcimar <alcimar.zeneide@gmail.com>
2022-10-08 13:39:05 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+
+import br.com.dio.models.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+//        Gato gato = new Gato();
+
+        int a = 5;
+        int b = 4;
+        int resultado = a + b;
+
+
+        System.out.println("Hello World!");
+        System.out.println("Resultado da soma é " + resultado);
+    }
+}
